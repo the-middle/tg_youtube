ab31d42fdfdc2d687a9b3bfcc50eca415e9dd428
the-middle <middle.oo.oo.ak@gmail.com>
2021-06-06 01:00:20 +0300

    Added yourube API search and downloading songs.

--- bot_youtube.py
+++ bot_youtube.py
@@ -1,11 +1,22 @@
-from tg_token import BOT_TOKEN
-import requests
+# import argparse
 import time
+
+import requests
+from googleapiclient.discovery import build
+from pytube import YouTube
+
+from g_api import google_api_key
+from tg_token import BOT_TOKEN
+import re
 
 tg_url = "https://api.telegram.org/bot"
 offset = 0
+DEVELOPER_KEY = google_api_key
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
-def sendMessage(i_text, i_chat_id):
+
+def sendMessage(i_text, i_chat_id, link_yt=""):
     method = "/sendMessage?"
     print(i_chat_id)
     if i_text == "/start":
@@ -16,8 +27,18 @@ def sendMessage(i_text, i_chat_id):
                 "text": i_text
             }
         )
+    else:
+        tg_send_message = requests.post(
+            url=tg_url + BOT_TOKEN + method,
+            json={
+                "chat_id": i_chat_id,
+                "parse_mode": "HTML",
+                "text": i_text + F"<a href=\"{link_yt}\">\nСсылка</a>"
+            }
+        )
         print(i_chat_id)
         print(tg_send_message.json())
+
 
 def getUpdates():
     global offset
@@ -33,34 +54,43 @@ def getUpdates():
     print(offset)
     print(tg_request_update.json())
     try:
-        print(message["result"][0]["message"]["text"])
-        sendMessage(message["result"][0]["message"]["text"], message["result"][0]["message"]["chat"]["id"])
+        youtube_search(message["result"][0]["message"]["text"],
+                       message["result"][0]["message"]["chat"]["id"])
     except:
         pass
-# def botRequest():
-#     tg_url = "https://api.telegram.org/bot"
-#     method = "/sendMessage?"
-#     if getImg() != 'Нет фото.':
-#         tg_request = requests.post(
-#             url=tg_url + token + method,
-#             json={
-#                 "chat_id": "@wiki_shit",
-#                 "parse_mode": "HTML",
-#                 "text": getText() + F"<a href=\"{getImg()}\">&#8205;</a>" + F"<a href=\"{getURL()}\">\nСсылка</a>",
-#                 "disable_web_page_preview": False
-#             }
-#         )
-#     else:
-#         tg_request = requests.post(
-#             url=tg_url + token + method,
-#             json={
-#                 "chat_id": "@wiki_shit",
-#                 "parse_mode": "HTML",
-#                 "text": getText() + F"<a href=\"{getURL()}\">\nСсылка</a>",
-#                 "disable_web_page_preview": True
-#             }
-#         )
-#     print(tg_request.text)
+
+
+def youtube_search(options, i_chat_id):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+
+    # Call the search.list method to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=options,
+        part='id,snippet',
+        maxResults=5,
+        type='video'
+    ).execute()
+
+    videos = []
+    yt_url = "https://www.youtube.com/watch?v="
+
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                       search_result['id']['videoId']))
+            sendMessage(search_result['snippet']['title'], i_chat_id,
+                        yt_url + search_result['id']['videoId'])
+            print(search_result['snippet']['title'], i_chat_id)
+            ytAudioDownload(yt_url + search_result['id']['videoId'])
+
+def ytAudioDownload(url_yt):
+    yt = YouTube(url_yt)
+    yt_stream = yt.streams.get_audio_only()
+    yt_stream.download()
+
+    print(yt_stream)
 
 if __name__ == "__main__":
     while True:
